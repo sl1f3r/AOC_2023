011152f8b67272de058ec366c288a241e57b1283
sl1f3r <sl1f3r@kaibanet.xyz>
2023-12-06 14:02:16 -0500

    added comments

--- day1.py
+++ day1.py
@@ -4,24 +4,24 @@ class Solution:
         self.total = 0
 
     def calcvalue(self):
-        file = open(self.input)
+        file = open(self.input) # read file 
 
-        for line in file: 
+        for line in file: # line by line through file
             numarray = []
             val = 0
             for character in line: 
-                if character.isnumeric():
+                if character.isnumeric(): # check which characters in that line are numbers
                     print(character)
-                    numarray.append(character)
+                    numarray.append(character) # add to array per line
             
-            if len(numarray) == 1: 
+            if len(numarray) == 1: # if length is one, that num is first and last
                 strval = str(numarray[0]) + str(numarray[0])
                 val = int(strval)
-            elif len(numarray) > 1: 
+            elif len(numarray) > 1: # if not, add the first and last
                 strval = str(numarray[0]) + str(numarray[-1])
                 val = int(strval)
             
-            self.total += val
+            self.total += val # add to total
 
 
         return self.total
