61fcdabbdd7accf72edb9f19d10de494484ddab7
sl1f3r <sl1f3r@kaibanet.xyz>
2023-12-09 17:42:18 -0500

    starting day 2

--- day2.py
+++ day2.py
@@ -1,2 +1,11 @@
+class Solution: 
+    def __init__(self):
+        self.input = '/Users/thom/Code/AOC_2023/day1.txt'
+        self.total = 0
+    
+    def check_possibility(self) -> bool: 
+        return 0
 
+    def solve(self):
+        return 0
 
