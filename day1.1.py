2a70334a20ce124cb99bc1e29fc8248c5f452d24
sl1f3r <sl1f3r@kaibanet.xyz>
2023-12-09 17:36:43 -0500

    fingers crossed day 1 is solved

--- day1.1.py
+++ day1.1.py
@@ -1,3 +1,6 @@
+import collections
+import re
+
 class Solution: 
     def __init__(self):
         self.input = '/Users/thom/Code/AOC_2023/day1.txt'
@@ -6,23 +9,31 @@ class Solution:
                     'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10}
 
     def calcvalue(self, line): # calculates the numerical value of a line
-        value = 0
-        numarray = []
+        val = 0
+        numdict = {}
         for i in range(len(line)): 
             if line[i].isnumeric(): # check which characters in that line are numbers
-                numarray.append(line[i]) # add to array per line
+                numdict[i] = line[i]
         
-        return value
+        for key in self.map.keys():
+            instances = [m.start() for m in re.finditer(key, line)]
+            if instances:
+                for ins in instances: 
+                    numdict[ins] = self.map[key]
+
+        od = collections.OrderedDict(sorted(numdict.items()))
+        strval = str(list(od.items())[0][1]) + str(list(od.items())[-1][1])
+        val = int(strval)
+
+        return val
 
     def read_file(self): 
         file = open(self.input)
         for line in file: 
-            value = self.calcvalue(line)
+            value = self.calcvalue(line) # iterate through file, get the value of each line, and add up
             self.total += value
 
         return self.total
-
-        
-            
-
-        
+    
+sol = Solution()
+print(sol.read_file())
