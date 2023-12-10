dce90a4fcd58235c1c3aed414095b62a247d9f2f
sl1f3r <sl1f3r@kaibanet.xyz>
2023-12-10 17:38:31 -0500

    day 2 part 1 solved

--- day2.py
+++ day2.py
@@ -2,7 +2,7 @@ import re
 
 class Solution: 
     def __init__(self):
-        self.input = '/Users/thom/Code/AOC_2023/day1.txt'
+        self.input = '/Users/thom/Code/AOC_2023/day2.txt'
         self.total = 0
         self.game = 1
         self.map = {'red': 12, 'green': 13, 'blue': 14}
@@ -15,7 +15,7 @@ class Solution:
             fo_befo = i - 4
             subslice = line[fo_befo: i]
             for item in subslice: 
-                if item.isinteger(): numstring = numstring + item
+                if item.isnumeric(): numstring = numstring + str(item)
 
             values.append(int(numstring))
 
@@ -25,12 +25,21 @@ class Solution:
     def check_possibility(self, line): 
         possible = True
 
-        green = self.get_values('green', line)# figure out green values
-        red = self.get_values('red', line)# figure out red values
-        blue = self.get_values('blue', line) # figure out blue values
+        greenvals = self.get_values('green', line)# figure out green values
+        redvals = self.get_values('red', line)# figure out red values
+        bluevals = self.get_values('blue', line) # figure out blue values
+
+        for g in greenvals:
+            if g > self.map['green']: possible = False
+        for r in redvals: 
+            if r > self.map['red']: possible = False
+        for b in bluevals: 
+            if b > self.map['blue']: possible = False
 
         # compare and decide
-        if possible: self.game += 1
+        if possible: self.total += self.game
+
+        self.game += 1
                 
 
 
@@ -39,7 +48,7 @@ class Solution:
         for line in file: 
             self.check_possibility(line)
         
-        print(self.total)
+        return self.total
 
 sol = Solution()
 print(sol.solve())
